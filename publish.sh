3208fb196c3ecb5dfdd26711e81e491fc8268114
Harald Glatt <code@hach.re>
2014-06-16 05:30:44 +0200

    0.4.20140616.5
    added: hachreAliases command to check for presence and the version of installed hachreAliases

--- publish.sh
+++ publish.sh
@@ -4,8 +4,8 @@
 
 nano version.txt
 version=`cat version.txt | head -n 1`
-linenr=`cat -n source/aliases.sh | grep "Version:" | head -n 1 | awk '{ print $1 }'`
-sed "${linenr}s/.*/# Version: ${version}/g" source/aliases.sh > aliases-new.sh
+linenr=`cat -n source/aliases.sh | grep "hachreAliasesVersion" | head -n 1 | awk '{ print $1 }'`
+sed "${linenr}s/.*/hachreAliasesVersion=${version}/g" source/aliases.sh > aliases-new.sh
 mv aliases-new.sh source/aliases.sh
 source source/aliases.sh
 gitit
