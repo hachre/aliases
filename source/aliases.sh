3e6dc072db91e82ca0509cf9775be8ccc44ecfe3
Harald Glatt <code@hach.re>
2014-06-21 07:11:54 +0200

    0.21.20140621.2
    fixed: pmsetup didn't create a user with a valid homedir

--- source/aliases.sh
+++ source/aliases.sh
@@ -4,7 +4,7 @@
 # Author: Harald Glatt code@hachre.de
 # URL: https://github.com/hachre/aliases
 # Version:
-hachreAliasesVersion=0.21.20140621.1
+hachreAliasesVersion=0.21.20140621.2
 
 #
 ### hachreAliases internal stuff
@@ -256,7 +256,7 @@ function setupArchAliases() {
 	function pmsetup() {
 		echo "Proceeding to set up hachre Arch Build system..."
 		$hachreAliasesRoot groupadd -g 500 archbuild
-		$hachreAliasesRoot useradd -u 500 -g 500 archbuild
+		$hachreAliasesRoot useradd -u 500 -g 500 -d /dev/null archbuild
 		pmii --needed --noconfirm sudo curl binutils base base-devel
 		$hachreAliasesRoot echo "archbuild ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 		mytemp=`$hachreAliasesRoot mktemp`
