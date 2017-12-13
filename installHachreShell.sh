ce4d9bb4934a910112706ba1a829a397c2fbf305
Harald Glatt <code@hach.re>
2017-12-13 06:55:44 +0100

    0.133.20171213.6
    changed: adapted install script to accommodate for move away from zstd

--- installHachreShell.sh
+++ installHachreShell.sh
@@ -6,17 +6,16 @@ rm install.sh
 source /usr/local/hachre/aliases/source/aliases.sh
 
 dyx
-dyi -y git zsh byobu mosh htop zstd
+dyi -y git zsh byobu mosh htop
 rehash 1>/dev/null 2>&1
 
 cd /usr/local/hachre/aliases
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 cd /tmp
-wget -q https://raw.githubusercontent.com/hachre/aliases/master/root-skel.tar.zst
-zstd -d root-skel.tar.zst && rm root-skel.tar.zst
+wget -q https://raw.githubusercontent.com/hachre/aliases/master/root-skel.tar.gz
 cd /root
-tar xvf /tmp/root-skel.tar
+tar xvzf /tmp/root-skel.tar && rm /tmp/root-skel.tar
 rm /tmp/root-skel.tar
 
 chsh -s `which zsh`
