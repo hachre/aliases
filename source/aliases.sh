22858059097cbc68cba077b27c951cc812c92e42
Harald Glatt <code@hach.re>
2017-12-15 06:43:32 +0100

    0.133.20171215.2
    fixed: zsh didn't save history on some platforms

--- source/aliases.sh
+++ source/aliases.sh
@@ -4,7 +4,7 @@
 # Author: Harald Glatt, code at hach.re
 # URL: https://github.com/hachre/aliases
 # Version:
-hachreAliasesVersion=0.133.20171215.1
+hachreAliasesVersion=0.133.20171215.2
 
 #
 ### hachreAliases internal stuff
