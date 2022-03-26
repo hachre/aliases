db67062b4060888ebac5e64f6f2c1da93adf311b
Harald Glatt <code@hach.re>
2022-03-26 13:02:42 +0100

    0.175.20220326.2

--- source/aliases.sh
+++ source/aliases.sh
@@ -4,7 +4,7 @@
 # Author: Harald Glatt, code at hach.re
 # URL: https://github.com/hachre/aliases
 # Version:
-hachreAliasesVersion=0.175.20220326.1
+hachreAliasesVersion=0.175.20220326.2
 
 #
 ### hachreAliases internal stuff
